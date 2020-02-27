b7c373afac51e6ab66991ec754cc59b8aaebc0f6
davidtheblane <davidtheblane@gmail.com>
2020-02-27 11:30:31 -0300

    configurado receber dados dos donors

--- server.js
+++ server.js
@@ -8,12 +8,24 @@ server.use(express.static("public"));
 //configurando a template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
-  express: server
+  express: server,
+  noCache: true // não fazer cache
 });
+
+// lista de doadores: Vetor ou Array
+const donors = [
+  { name: "Davi Bernardo", blood: "AB+" },
+
+  { name: "Cleiton Souza", blood: "O+" },
+
+  { name: "Dayane Bartolomeu", blood: "B+" },
+
+  { name: "Fausto Silva", blood: "A-" }
+];
 
 //configurar a apresentação da pagina
 server.get("/", function(req, res) {
-  return res.render("index.html");
+  return res.render("index.html", { donors });
 });
 
 //ligar o servidor e permitir o acesso na porta 3000
