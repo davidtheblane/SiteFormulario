c630b54134d952f0e60b45680937fe778eceae45
davidtheblane <davidtheblane@gmail.com>
2020-02-27 11:42:52 -0300

    configurado recebimento de dados do formulario, prox passo BD

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = express();
 //configurar o servidor para apresentar arquivos estaticos
 server.use(express.static("public"));
 
+// habilitar body do formulario
+server.use(express.urlencoded({ extended: true }));
+
 //configurando a template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
@@ -26,6 +29,21 @@ const donors = [
 //configurar a apresentação da pagina
 server.get("/", function(req, res) {
   return res.render("index.html", { donors });
+});
+
+server.post("/", function(req, res) {
+  //pegar dados do formulario.
+  const name = req.body.name;
+  const email = req.body.email;
+  const blood = req.body.blood;
+
+  // coloco valores dentro do array
+  donors.push({
+    name: name,
+    blood: blood
+  });
+
+  return res.redirect("/");
 });
 
 //ligar o servidor e permitir o acesso na porta 3000
